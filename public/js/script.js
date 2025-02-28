d126e46c2bce74a5cfae10944bf25e89286cef9b
Miketerrara <michaeldrums19@gmail.com>
2025-02-28 10:06:51 -0300

    update css and javascript

--- public/js/script.js
+++ public/js/script.js
@@ -7,9 +7,10 @@ const elements = {
     durafloorLine: document.getElementById('linha-durafloor'),
     espacofloorLine: document.getElementById('linha-espacofloor'),
     quickLine: document.getElementById('linha-quickstep'),
-    tarketLine: document.getElementById('linha-tarket')
+    tarketLine: document.getElementById('linha-tarket'),
+    brandsMobile: document.getElementById('list-brands'),
+    photoSelection: document.querySelector('.block-pisos__principal--selection')
 }
-
 elements.bar.addEventListener('click', () =>{
     elements.barScreen.classList.toggle('bar-modal--active');
 });
@@ -17,6 +18,18 @@ elements.bar.addEventListener('click', () =>{
 elements.closeBarScreen.addEventListener('click', () => {
     elements.bar.dispatchEvent(new Event('click'));
 })
+const display = {
+    eucafloorScreen: document.getElementById('eucafloor'),
+    durafloorScreen: document.getElementById('durafloor'),
+    espacofloorScreen: document.getElementById('espacofloor'),
+    quickstepScreen: document.getElementById('quickstep'),
+    tarketScreen: document.getElementById('tarket'),
+    eucafloorGallery: document.getElementById('gallery_eucafloor'),
+    durafloorGallery: document.getElementById('gallery_durafloor'),
+    espacofloorGallery: document.getElementById('gallery_espacofloor'),
+    quickstepGallery: document.getElementById('gallery_quickstep'),
+    tarketGallery: document.getElementById('gallery_tarket')
+}
 
 let eucafloor = elements.eucafloorLine;
 let durafloor = elements.durafloorLine;
@@ -24,50 +37,157 @@ let espacofloor = elements.espacofloorLine;
 let quickstep = elements.quickLine;
 let tarket = elements.tarketLine;
 
-let children = elements.brandsSelect.querySelectorAll('*');
+let screenEucafloor = Array.from(display.eucafloorScreen.querySelectorAll('*'));
+
+let screen = [
+    display.durafloorScreen,
+    display.espacofloorScreen,
+    display.eucafloorScreen,
+    display.quickstepScreen,
+    display.tarketScreen
+]
+
+let gallery = [
+    display.durafloorGallery,
+    display.espacofloorGallery,
+    display.eucafloorGallery,
+    display.quickstepGallery,
+    display.tarketGallery
+]
+
+function updateChildren() {
+    if (window.innerWidth <= 767) {
+        children = elements.brandsMobile.querySelectorAll('*');
+    } else {
+        children = elements.brandsSelect.querySelectorAll('*');
+    }
+}
+updateChildren();
+
+window.addEventListener('resize', updateChildren);
 let arrayChildren = Array.from(children);
-arrayChildren.forEach((item, index) => {
-    item.addEventListener('click', () => {
-        arrayChildren.forEach(otherItem => otherItem.classList.remove('item-brand--active'));
-        item.classList.add('item-brand--active');
-        console.log(index);
-        
-        let eucafloor = elements.eucafloorLine;
-        let durafloor = elements.durafloorLine;
-        let espacofloor = elements.espacofloorLine;
-        let quickstep = elements.quickLine;
-        let tarket = elements.tarketLine;
-        
-        eucafloor.style.display = 'none';
-        durafloor.style.display = 'none';
-        espacofloor.style.display = 'none';
-        quickstep.style.display = 'none';
-        tarket.style.display = 'none';
-        
-        switch(index) {
-            case 0:
-            durafloor = durafloor.style.display = 'flex';
-            break
-            case 2: 
-            espacofloor = espacofloor.style.display = 'flex';
-            break
-            case 3: 
-            eucafloor = eucafloor.style.display = 'flex';
-            break
-            case 5:
-            quickstep = quickstep.style.display = 'flex';
-            break
-            case 6:
-            tarket = tarket.style.display = 'flex';
-        }
-        
+
+function clickBrands () {
+    arrayChildren.forEach((item, index) => {
+        item.addEventListener('click', () => {
+            arrayChildren.forEach(otherItem => {
+                otherItem.classList.remove('item-brand--active');
+                screen.forEach(anotherItem => anotherItem.style.display = 'none');
+                gallery.forEach(galleryItem => galleryItem.style.display = 'none');
+            });
+            item.classList.add('item-brand--active');
+            /* let eucafloor = elements.eucafloorLine;
+            let durafloor = elements.durafloorLine;
+            let espacofloor = elements.espacofloorLine;
+            let quickstep = elements.quickLine;
+            let tarket = elements.tarketLine;*/
+            
+            eucafloor.style.display = 'none';
+            durafloor.style.display = 'none';
+            espacofloor.style.display = 'none';
+            quickstep.style.display = 'none';
+            tarket.style.display = 'none';
+            
+            let activeScreen = null;
+            
+            switch(index) {
+                case 0:
+                durafloor.style.display = 'flex';
+                activeScreen = screen[0]
+                activeScreen.style.display = 'flex'
+                galleryNumber = gallery[0];
+                galleryNumber.style.display = 'flex';
+                break
+                case 2: 
+                espacofloor.style.display = 'flex';
+                activeScreen = screen[1]
+                activeScreen.style.display = 'flex'
+                galleryNumber = gallery[1]
+                galleryNumber.style.display = 'flex';
+                break
+                case 3: 
+                eucafloor.style.display = 'flex';
+                activeScreen = screen[2]
+                activeScreen.style.display = 'flex'
+                galleryNumber = gallery[2];
+                galleryNumber.style.display = 'flex';
+                break
+                case 5:
+                quickstep.style.display = 'flex';
+                activeScreen = screen[3]
+                activeScreen.style.display = 'flex'
+                galleryNumber = gallery[3];
+                galleryNumber.style.display = 'flex';
+                break
+                case 6:
+                tarket.style.display = 'flex';
+                activeScreen = screen[4]
+                activeScreen.style.display = 'flex';
+                galleryNumber = gallery[4];
+                galleryNumber.style.display = 'flex';
+                break
+                default:
+                console.warn('Indice inválido: ', index);
+            }
+            console.log(galleryNumber.children);
+            let filhosdaMae = Array.from(galleryNumber.children);
+            filhosdaMae.forEach((teste, index) => {
+                teste.addEventListener('click', () =>{
+                    let floorPrincipal = activeScreen.querySelectorAll('*');
+                    console.log(floorPrincipal[index].src)
+                })
+            })
+            function prevImage() {
+                if(!activeScreen) return;
+                
+                let prev = document.querySelector('[data-target="prev"]');
+                let images = Array.from(activeScreen.querySelectorAll('*')); 
+                let currentIndex = images.length - 1; 
+                
+                prev.addEventListener('click', () => {
+                    images.forEach(img => img.style.display = 'none');
+                    currentIndex = (currentIndex - 1 + images.length) % images.length; 
+                    images[currentIndex].style.display = 'block';
+                });
+            }        
+            function nextImage() {
+                if (!activeScreen) return;
+                let next = document.querySelector('[data-target="next"]');
+                let images = Array.from(activeScreen.querySelectorAll('*')); // Obtém todas as imagens dentro de screen[1]
+                let currentIndex = 0; // Começa na primeira imagem
+                
+                next.addEventListener('click', () => {
+                    // Esconde todas as imagens
+                    images.forEach(img => img.style.display = 'none');
+                    
+                    // Atualiza o índice para a próxima imagem
+                    currentIndex = (currentIndex + 1) % images.length;
+                    
+                    // Exibe a imagem atual
+                    images[currentIndex].style.display = 'block';
+                });
+            }
+            prevImage();
+            nextImage();
+        });
     });
-});
+}
+function switchBrands() {
+    
+}
+
+        
+        
+clickBrands();
 let typeEucafloor = Array.from(eucafloor.getElementsByTagName('li'));
 let typeDurafloor = Array.from(durafloor.getElementsByTagName('li'));
 let typeEspacofloor = Array.from(espacofloor.getElementsByTagName('li'));
 let typeQuickstep = Array.from(quickstep.getElementsByTagName('li'));
 let typeTarket = Array.from(tarket.getElementsByTagName('li'));
+
+
+console.log();
+
 
 function handleType(type, index) {
     let paragraphType = Array.from(type.querySelectorAll('p, strong'));
@@ -93,13 +213,11 @@ function handleType(type, index) {
             paragraphTypeblack.forEach(paragraph => {paragraph.style.color = '#3a3a3a'})
         });
         paragraphType.forEach(paragraph => {paragraph.style.color = '#93613E'});
-        console.log(index);
     });
 }
 
 typeEucafloor.forEach((type, index) =>{
     handleType(type, index);
-    // console.log(type.getElementsByTagName('p'))
 });
 
 typeDurafloor.forEach((type, index) =>{
@@ -114,5 +232,3 @@ typeQuickstep.forEach((type, index) =>{
 typeTarket.forEach((type, index) =>{
     handleType(type, index);
 })
-// console.log(eucafloor.getElementsByTagName('li'))
-// console.log(durafloor.getElementsByTagName('li'))
